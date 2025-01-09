9148a6c115f06fb0b66e444879dc7dd807faef66
JackSwitzer <jackswitzer3@gmail.com>
2025-01-09 09:41:15 -0500

    bugfix?

--- app/tutorials/page.tsx
+++ app/tutorials/page.tsx
@@ -8,14 +8,16 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Tutorial Problems</h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <ul>
-          <li>Week 1
-            <ul>
-              <li><a href="/tutorials/week1.pdf">Tutorial 1 Problems</a></li>
-              <li><a href="/tutorials/week1-solutions.pdf">Tutorial 1 Solutions</a></li>
-            </ul>
-          </li>
-        </ul>
+        <h2 className="text-xl font-medium">
+          Week 1:{' '}
+          <a href="/tutorials/week1/Tutorial1Questions.pdf" className="text-blue-500 hover:text-blue-600">
+            Problems
+          </a>
+          {' | '}
+          <a href="/tutorials/week1/Tutorial1Solutions.pdf" className="text-blue-500 hover:text-blue-600">
+            Solutions
+          </a>
+        </h2>
       </div>
     </section>
   )
