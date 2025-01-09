69e17baec59c0df971390a1456fe550f3b5b1581
JackSwitzer <jackswitzer3@gmail.com>
2025-01-08 21:00:40 -0500

    Youtube video tesrt

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,3 +1,5 @@
+import { YouTube } from '../components/youtube'
+
 export const metadata = {
   title: 'YouTube Videos',
   description: 'Educational content and tutorials.',
@@ -7,7 +9,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">YouTube Videos</h1>
-      {/* Content will go here */}
+      <YouTube 
+        id="fNk_zzaMoSs" 
+        title="Vectors, what even are they? | Essence of Linear Algebra, Chapter 1" 
+      />
     </section>
   )
 } 
