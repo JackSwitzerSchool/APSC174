ff99b940182ac3fc2709ad336b27798ad40d6e66
JackSwitzer <jackswitzer3@gmail.com>
2025-01-12 19:34:28 -0500

    test

--- types/remark-wiki-link.d.ts
+++ types/remark-wiki-link.d.ts
@@ -1,4 +1,14 @@
 declare module 'remark-wiki-link' {
-  const remarkWikiLink: any
+  import type { Plugin } from 'unified'
+
+  interface WikiLinkOptions {
+    pageResolver?: (name: string) => string[]
+    hrefTemplate?: (permalink: string) => string
+    aliasDivider?: string
+    wikiLinkClassName?: string
+    newClassName?: string
+  }
+
+  const remarkWikiLink: Plugin<[WikiLinkOptions?]>
   export = remarkWikiLink
 }
